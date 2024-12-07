cd452db79d5d9fddc5e2d3c20bf55fa729acd9aa
Sai Navadeep <2200030032@kluniversity.in>
2024-12-08 03:28:37 +0530

    API Changes

--- src/config/api.js
+++ src/config/api.js
@@ -1,9 +1,9 @@
 
 import axios from 'axios';
 // const DEPLOYED='https://e-commerce-server-production-0873.up.railway.app'
-const LOCALHOST='http://localhost:2024'
+// const LOCALHOST='http://localhost:2024'
 
-export const API_BASE_URL = "tribesnv-backend-production.up.railway.app"
+export const API_BASE_URL = "https://tribesnv-backend-production.up.railway.app"
 
 const api = axios.create({
   baseURL: API_BASE_URL,
